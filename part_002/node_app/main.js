2e63fbe8d6cfe8196989365e858b053d389f39b9
kush kumar <kushkumarking098765@gmail.com>
2025-09-21 10:55:30 +0530

    Initialize Node.js application with package.json and express dependency

---
+++ part_002/node_app/main.js
@@ -0,0 +1,11 @@
+import  express from 'express';
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello, World!' });
+});
+
+app.listen(port , () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
